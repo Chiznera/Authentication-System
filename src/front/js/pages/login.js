7416fcd18a4e9aad4d240c483199ad420db3f0ed
Chiznera <chiznera@gmail.com>
2022-10-04 21:48:10 +0000

    login complete, needs logout function

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (store.logStatus) {
+      navigate("/private");
+    }
+  }, [store.logStatus]);
+
+  return (
+    <div className="container">
+      <form className="my-5">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            onChange={(e) => setPassword(e.target.value)}
+            id="exampleInputPassword1"
+          />
+        </div>
+        {/* <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div> */}
+        <div
+          //   type="submit"
+          class="btn btn-primary"
+          onClick={() => actions.login(email, password)}
+        >
+          Submit
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,29 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  let backendUrl =
+    "https://3001-chiznera-authentication-oei0x7z8zk4.ws-us67.gitpod.io";
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  return {
+    store: {
+      logStatus: false,
+    },
+    actions: {
+      login: (email, password) => {
+        let requestOptions = {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({ email: email, password: password }),
+        };
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        fetch(backendUrl + "/api/login", requestOptions)
+          .then((response) => response.text())
+          .then((result) => {
+            console.log(result);
+            setStore({ logStatus: true });
+          })
+          .catch((error) => console.log("error", error));
+      },
+    },
+  };
 };
 
 export default getState;
